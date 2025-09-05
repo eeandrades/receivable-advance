be82d2333c455f1d36107055dd4eb8a9e97b5c65
Emerson Andrade <emerson.andrade@outlook.com>
2025-09-05 11:40:41 -0300

    feat: create a endpoint to create a new Receivable Advance Request

---
+++ src/ReceivableAdvance.WebApi/Endpoints/ReceivableAdvanceRequests/CreateReceivableAdvanceRequests/CreateReceivableAdvanceRequestEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReceivableAdvance.Application.Commands.CreateReceivableAdvanceRequests;
+using ReceivableAdvance.WebApi.Common.Notifications;
+
+namespace ReceivableAdvance.WebApi.Endpoints.ReceivableAdvanceRequests.CreateReceivableAdvanceRequests;
+
+public static class CreateReceivableAdvanceRequestEndpoint
+{
+    public record Request(Guid CreatorId, decimal RequestAmount)
+    {
+        public static implicit operator CreateReceivableAdvanceRequestCommand(Request request) => new(request.CreatorId, request.RequestAmount);
+    }
+
+    public record Response(Guid ReceivableAdvanceRequestId, DateTime RequestDate, Decimal NetAmmount)
+    {
+        public static implicit operator Response(CreateReceivableAdvanceRequestResult result) => new(result.ReceivableAdvanceRequestId, result.RequestDate, result.NetAmmount);
+    }
+
+    private static async Task<IResult> ExecuteAsync([FromBody] Request request, [FromServices] ICreateReceivableAdvanceRequestHandler handler)
+    {
+        var result = await handler.ExecuteAsync(request);
+        return result.MapMinimalApiResult(value => value != null ? (Response)value : default);
+    }
+
+    public static RouteGroupBuilder MapCreateReceivableAdvanceRequestEndpoint(this RouteGroupBuilder group)
+    {
+        group
+            .MapPost("/advance-requests", ExecuteAsync)
+            .Produces<Response>(StatusCodes.Status201Created)
+            .WithOpenApi();
+        return group;
+    }
+}
---
+++ src/ReceivableAdvance.WebApi/Endpoints/ReceivableAdvanceRequests/ReceivableAdvanceRequestsGroup.cs
@@ -0,0 +1,25 @@
+﻿using Asp.Versioning.Builder;
+using ReceivableAdvance.WebApi.Endpoints.ReceivableAdvanceRequests.CreateReceivableAdvanceRequests;
+
+namespace ReceivableAdvance.WebApi.Endpoints.ReceivableAdvanceRequests;
+
+public static class ReceivableAdvanceRequestsGroup
+{
+    public static IEndpointRouteBuilder MapReceivableAdvanceRequestsGroup(this IEndpointRouteBuilder routes, ApiVersionSet versions)
+    {
+        routes.MapGroup("v{version:apiVersion}/receivable-advance-requests")
+            .WithTags("ReceivableAdvanceRequests")
+            .WithApiVersionSet(versions)
+            .HasApiVersion(new(1, 0))
+            .MapCreateReceivableAdvanceRequestEndpoint();
+
+
+
+        //group.MapGet("/", GetAllReceivableAdvanceRequestsEndpoint.Handle);
+        //group.MapGet("/{id:guid}", GetReceivableAdvanceRequestByIdEndpoint.Handle);
+        //group.MapPost("/", CreateReceivableAdvanceRequestEndpoint.Handle);
+        //group.MapPut("/{id:guid}", UpdateReceivableAdvanceRequestEndpoint.Handle);
+        //group.MapDelete("/{id:guid}", DeleteReceivableAdvanceRequestEndpoint.Handle);
+        return routes;
+    }
+}
