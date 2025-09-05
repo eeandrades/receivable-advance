4928db765cd16908c374340652e7325b2d980d3e
Emerson Andrade <emerson.andrade@outlook.com>
2025-09-05 11:36:26 -0300

    feat: Create data context, to help database operations

---
+++ src/ReceivableAdvance.Infra/Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using System.Data.Common;
+
+namespace ReceivableAdvance.Infra.Data;
+
+public class DataContext
+{
+    private readonly Lazy<DbConnection> _lazyConnection;
+    private readonly DbProviderFactory _factory;
+    private readonly DbConnectionStringBuilder _connectionStringBuilder;
+
+    public DataContext(DbProviderFactory factory, DbConnectionStringBuilder connectionStringBuilder)
+    {
+        _lazyConnection = new Lazy<DbConnection>(CreateConnection);
+        _factory = factory;
+        _connectionStringBuilder = connectionStringBuilder;
+    }
+
+    public DbConnection Connection => _lazyConnection.Value;
+    private DbConnection CreateConnection()
+    {
+        var result = _factory.CreateConnection() ?? throw new InvalidOperationException("Failed to create a connection.");
+
+        result.ConnectionString = _connectionStringBuilder.ConnectionString;
+
+        return result;
+    }
+}
